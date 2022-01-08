c25c4de0e74e7cef4255045c040556d1c0628083
Kushal2001 <kushalthegreat90@gmail.com>
2022-01-08 16:44:36 +0530

    Initial commit

---
+++ modifyblockchain.js
@@ -0,0 +1,52 @@
+const SHA256 = require("crypto-js/sha256");
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = '';
+    }
+    calulateHash() {
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+    createGenesisBlock() {
+        return new Block(0, "01/01/2021", "Genesis Block", "0");
+    }
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calulateHash();
+        this.chain.push(newBlock);
+    }
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+            if (currentBlock.hash !== currentBlock.calulateHash()) {
+                return false;
+            }
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+let bchain = new Blockchain();
+bchain.addBlock(new Block(1, "25/04/2021", { amount: 1 }));
+bchain.addBlock(new Block(2, "27/04/2021", { amount: 1 }));
+bchain.addBlock(new Block(3, "30/04/2021", { amount: 1 }));
+console.log('Before modification:');
+console.log('Is blockchain valid?: ' + bchain.isChainValid());
+bchain.chain[1].data = { amount: 100 };
+bchain.chain[1].hash = bchain.chain[1].calulateHash();
+console.log('After modification:');
+console.log('Is blockchain valid?: ' + bchain.isChainValid());
